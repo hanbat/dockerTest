5ac3f8c60fd76f1eb87314220c4a133ee8190363
HBKil <hanbatkil@Hanbats-iMac-2.local>
2015-03-23 16:58:35 +0900

    count now 4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get(['/', '/index.html'], function (req, res) {
-  res.send('Hello Docker changed by git push and updating with script count: 3');
+  res.send('Hello Docker changed by git push and updating with script count: 4');
 });
 
 app.listen(80);
