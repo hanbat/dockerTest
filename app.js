bd63d52306a0e1321611dae83a49edba011533d8
HBKil <hanbatkil@Hanbats-iMac-2.local>
2015-03-23 15:46:35 +0900

    another updating test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get(['/', '/index.html'], function (req, res) {
-  res.send('Hello Docker changed by git push');
+  res.send('Hello Docker changed by git push and updating with script');
 });
 
 app.listen(80);
