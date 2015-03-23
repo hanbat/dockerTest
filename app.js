5b9069ad3df87052321ace1708cb5c04adf29277
HBKil <hanbatkil@Hanbats-iMac-2.local>
2015-03-23 18:15:56 +0900

    trying something new

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get(['/', '/index.html'], function (req, res) {
   res.send('Hello Docker changed by git push and updating with script count: 5');
 });
 
-app.get(['/test', '/index.html'], function (req, res) {
-  res.send('test Docker count: 6');
+app.get(['/test', '/index1.html'], function (req, res) {
+  res.send('test LOVES<3 Docker count: 6');
 });
 
 app.listen(80);
