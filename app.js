3ad5dbef41f2a431d6f08a83e52adb49853314a8
HBKil <hanbatkil@Hanbats-iMac-2.local>
2015-03-23 15:17:29 +0900

    edited txt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get(['/', '/index.html'], function (req, res) {
-  res.send('Hello Docker');
+  res.send('Hello Docker changed by git push');
 });
 
 app.listen(80);
